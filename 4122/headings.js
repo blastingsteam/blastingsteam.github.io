0ae64913f2258814dad10cfda8c352cadfe52843
quasar1742 <siddhantkhapra101@gmail.com>
2023-04-01 19:29:43 -0700

    4122

---
+++ 4122/headings.js
@@ -0,0 +1,48 @@
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+    	<header id="header" class="header clearfix"> 
+        	<div class="container">
+                <div class="header-wrap clearfix">
+                    <div id="logo" class="logo">
+                        <a href="index.html" rel="home">
+                            <img style="width: 1000px;" src="steamlogo.png" alt="image">
+                        </a>
+                    </div>           
+                    <div class="nav-wrap">
+                        <div class="btn-menu">
+                            <span></span>
+                        </div>
+                        <nav id="mainnav" class="mainnav">
+                            <ul class="menu"> 
+                                <li><a href="index.html">Home</a>
+                                    <li><a href="about-us.html">About</a></li>
+                                    <li><a href="classes.html">Our Classes</a>
+                                    <li><a href="contact.html">Contact</a>
+                                    <li><a href="register.html">Register</a>
+                                    </li>
+                                    <li><a href="news.html">News Scoops</a></li>
+                            </ul>
+                        </nav>   
+                    </div>
+
+                    <div id="s" class="show-search">
+                        <a href="#"><i class="fa fa-search"></i></a>         
+                    </div>
+                    
+                    <div class="submenu top-search">
+                        <div class="widget widget_search">
+                            <form class="search-form">
+                                <input type="search" class="search-field" placeholder="Search …">
+                                <input type="submit" class="search-submit">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        `
+    }
+}
+
+customElements.define('my-header', MyHeader)
